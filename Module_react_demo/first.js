2eb4d3718ff4a4a40cb6044e7f3753a962491a51
Nisha Saud <nishasaud2061@gmail.com>
2024-09-21 07:57:45 +0545

    added

---
+++ Module_react_demo/first.js
@@ -0,0 +1,8 @@
+export default   function add( a,b) //default export
+    {
+     console.log(a+b);
+    }
+    export function show()
+    {
+        console.log("Hello");
+    }
---
+++ Module_react_demo/second.js
@@ -0,0 +1,4 @@
+import add, {show}from "./first.js"; //here default export is imported first and after that thae named export 
+add(12,13);
+show();
+//default export can be exported as any name and without  any curly braces
